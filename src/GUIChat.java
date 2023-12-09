2a38a2996ede12a8c2cf10a791e9fd500abe1604
soocratess <socratessat@gmail.com>
2023-12-09 21:05:58 +0100

    Segunda versión GUI chat

--- src/GUIChat.java
+++ src/GUIChat.java
@@ -13,14 +13,15 @@ public class GUIChat {
     private JFrame frame;
     private JTextArea chatArea;
     private JTextField inputField;
-    private JButton sendButton; // Agregamos un botón de envío
+    private JButton sendButton;
     private Process process;
     private BufferedReader reader;
     private Timer timer;
-    private JPanel panel1;
     private ArrayList<String> nombres;
     private Map<String, String> mensajesPorUsuario;
     private JList<String> nombresList;
+    private JMenuBar menuBar;
+    private JPanel mainPanel;
 
     public GUIChat() {
         nombres = new ArrayList<>();
@@ -28,58 +29,94 @@ public class GUIChat {
         nombres.add("Alicia");
         nombres.add("Mauro");
 
-        frame = new JFrame("Terminal Chat");
-        chatArea = new JTextArea();
-        inputField = new JTextField();
-        sendButton = new JButton("Send"); // Creamos el botón
+        frame = new JFrame("P2P ChatApp");
+        chatArea = new JTextArea("¡Selecciona un chat para empezar a hablar!");
+        chatArea.setEditable(false);
+        inputField = new JTextField(20);
+        sendButton = new JButton("Send");
 
         mensajesPorUsuario = new HashMap<>();
         for (String nombre : nombres) {
-            mensajesPorUsuario.put(nombre, ""); // Inicializa cada chat vacío
+            mensajesPorUsuario.put(nombre, "");
         }
-        chatArea.setEditable(false);
 
-        // Utilizamos un JSplitPane para dividir la ventana en dos partes
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-        splitPane.setDividerLocation(100); // Ancho de la lista de nombres
+        splitPane.setDividerLocation(100);
         splitPane.setLeftComponent(createNombresPanel());
         splitPane.setRightComponent(createChatPanel());
 
-        frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+        menuBar = new JMenuBar();
+        JMenu chatMenu = new JMenu("Chat");
+        JMenuItem chatMenuItem = new JMenuItem("Abrir Chat");
+        JMenu listaNombresMenu = new JMenu("Amigos");
+        JMenuItem listaNombresMenuItem = new JMenuItem("Solicitudes pendientes");
+        JMenuItem amigosMenuItem = new JMenuItem("Amigos");
 
-        // Agregar un ListSelectionListener a nombresList
+        chatMenu.add(chatMenuItem);
+        listaNombresMenu.add(listaNombresMenuItem);
+        listaNombresMenu.add(amigosMenuItem);
+        menuBar.add(chatMenu);
+        menuBar.add(listaNombresMenu);
+
+        frame.setJMenuBar(menuBar);
+
+        mainPanel = new JPanel(new CardLayout());
+        mainPanel.add(splitPane, "ChatPanel");
+        mainPanel.add(createListaNombresPanel(), "ListaNombresPanel");
+        mainPanel.add(createAmigosPanel(), "AmigosPanel");
+
+        frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+
+        chatMenuItem.addActionListener(e -> cambiarVista("ChatPanel"));
+        listaNombresMenuItem.addActionListener(e -> cambiarVista("ListaNombresPanel"));
+        amigosMenuItem.addActionListener(e -> cambiarVista("AmigosPanel"));
+
+        // Agregar un apartado para enviar solicitud de amistad
+        JMenuItem enviarSolicitudAmistadItem = new JMenuItem("Enviar Solicitud de Amistad");
+        listaNombresMenu.add(enviarSolicitudAmistadItem);
+        enviarSolicitudAmistadItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Mostrar un cuadro de diálogo para ingresar el nombre
+                String nombreIngresado = JOptionPane.showInputDialog(frame, "Ingresa el nombre para la solicitud de amistad:");
+                if (nombreIngresado != null && !nombreIngresado.trim().isEmpty()) {
+                    String nombreSolicitado = nombreIngresado.trim();
+                    // Aquí puedes implementar la lógica adicional con el nombre ingresado
+                    System.out.println("Nombre solicitado: " + nombreSolicitado); // Ejemplo de impresión
+                }
+            }
+        });
+
+
+        nombresList.clearSelection(); // Asegurarse de que no haya selección inicial
         nombresList.addListSelectionListener(e -> {
             if (!e.getValueIsAdjusting()) {
                 String selectedName = nombresList.getSelectedValue();
-                chatArea.setText(mensajesPorUsuario.get(selectedName)); // Actualiza el chatArea
+                if (selectedName != null) {
+                    chatArea.setText(mensajesPorUsuario.get(selectedName));
+                } else {
+                    chatArea.setText("¡Selecciona un chat para empezar a hablar!");
+                }
             }
         });
 
-        // Agregamos un ActionListener al botón de envío
-        sendButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sendMessage(inputField.getText());
-            }
-        });
-
-        // Agregamos un ActionListener al inputField para manejar la tecla Enter
-        inputField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sendMessage(inputField.getText());
-            }
-        });
-
+        sendButton.addActionListener(e -> sendMessage(inputField.getText()));
+        inputField.addActionListener(e -> sendMessage(inputField.getText()));
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 300); // Ajusta el tamaño según tus preferencias
+        frame.setSize(600, 300);
+
+        // Centrar la ventana en la pantalla
+        frame.setLocationRelativeTo(null);
+
         frame.setVisible(true);
 
         startTerminalReader();
     }
 
+
     private JPanel createNombresPanel() {
         JPanel nombresPanel = new JPanel(new BorderLayout());
-        nombresList = new JList<>(nombres.toArray(new String[0])); // Usa la variable de instancia
+        nombresList = new JList<>(nombres.toArray(new String[0]));
         JScrollPane nombresScrollPane = new JScrollPane(nombresList);
         nombresPanel.add(nombresScrollPane, BorderLayout.CENTER);
         return nombresPanel;
@@ -89,7 +126,6 @@ public class GUIChat {
         JPanel chatPanel = new JPanel(new BorderLayout());
         JScrollPane chatScrollPane = new JScrollPane(chatArea);
 
-        // Creamos un JPanel para el campo de entrada y el botón
         JPanel inputPanel = new JPanel(new BorderLayout());
         inputPanel.add(inputField, BorderLayout.CENTER);
         inputPanel.add(sendButton, BorderLayout.EAST);
@@ -105,17 +141,14 @@ public class GUIChat {
             process = Runtime.getRuntime().exec("/bin/bash");
             reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-            // Leer continuamente la salida de la terminal
-            timer = new Timer(100, new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    try {
-                        if (reader.ready()) {
-                            String message = reader.readLine();
-                            appendMessage("terminal: ", message);
-                        }
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
+            timer = new Timer(100, e -> {
+                try {
+                    if (reader.ready()) {
+                        String message = reader.readLine();
+                        appendMessage("terminal: ", message);
                     }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
                 }
             });
             timer.start();
@@ -125,59 +158,91 @@ public class GUIChat {
     }
 
     private void sendMessage(String message) {
-        // Verifica si hay un mensaje para enviar y si hay un usuario seleccionado
         if (message.isEmpty() || nombresList.getSelectedValue() == null) {
             return;
         }
 
-        // Obtiene el nombre del usuario seleccionado
         String selectedName = nombresList.getSelectedValue();
-
-        // Obtiene el historial de chat actual para el usuario seleccionado
         String chatHistory = mensajesPorUsuario.getOrDefault(selectedName, "");
 
-        // Añade el nuevo mensaje al historial de chat
         chatHistory += "You: " + message + "\n";
-
-        // Actualiza el historial de chat en el HashMap
         mensajesPorUsuario.put(selectedName, chatHistory);
-
-        // Actualiza el área de chat en la GUI
         chatArea.setText(chatHistory);
 
-        // Limpia el campo de entrada de texto
         inputField.setText("");
-
-        // Aquí puedes agregar la lógica para enviar el mensaje a un servidor o a otro proceso, si es necesario
     }
 
-
     private void appendMessage(String sender, String message) {
-        // Obtiene el nombre del usuario seleccionado actualmente en la lista
         String selectedName = nombresList.getSelectedValue();
         if (selectedName != null) {
-            // Obtiene el historial de chat actual para el usuario seleccionado
             String chatHistory = mensajesPorUsuario.getOrDefault(selectedName, "");
-
-            // Añade el nuevo mensaje al historial de chat
             chatHistory += sender + ": " + message + "\n";
-
-            // Actualiza el historial de chat en el HashMap
             mensajesPorUsuario.put(selectedName, chatHistory);
-
-            // Actualiza el área de chat en la GUI solo si el mensaje es del usuario actualmente seleccionado
             chatArea.setText(chatHistory);
         }
     }
 
+    private void cambiarVista(String nombreVista) {
+        CardLayout cl = (CardLayout)(mainPanel.getLayout());
+        cl.show(mainPanel, nombreVista);
+    }
+
+    private JPanel createListaNombresPanel() {
+        // Panel que contendrá todos los nombres
+        JPanel listaNombresPanel = new JPanel();
+        listaNombresPanel.setLayout(new BoxLayout(listaNombresPanel, BoxLayout.Y_AXIS));
+
+        // Nombres a mostrar
+        String[] nombres = {"María", "Juan", "Roberto", "María", "Juan", "Roberto", "María", "Juan", "Roberto", "María", "Juan", "Roberto"};
+
+        // Crear un panel para cada nombre
+        for (String nombre : nombres) {
+            JPanel nombrePanel = new JPanel(new FlowLayout());
+            JLabel nombreLabel = new JLabel(nombre);
+            JButton aceptarButton = new JButton("Aceptar");
+            JButton rechazarButton = new JButton("Rechazar");
+
+            aceptarButton.addActionListener(e -> {
+                // Remueve el panel del nombre al presionar "Aceptar"
+                listaNombresPanel.remove(nombrePanel);
+                listaNombresPanel.revalidate();
+                listaNombresPanel.repaint();
+            });
+
+            rechazarButton.addActionListener(e -> {
+                // Remueve el panel del nombre al presionar "Rechazar"
+                listaNombresPanel.remove(nombrePanel);
+                listaNombresPanel.revalidate();
+                listaNombresPanel.repaint();
+            });
+
+            nombrePanel.add(nombreLabel);
+            nombrePanel.add(aceptarButton);
+            nombrePanel.add(rechazarButton);
+            listaNombresPanel.add(nombrePanel);
+        }
+
+        JScrollPane scrollPane = new JScrollPane(listaNombresPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        JPanel panelConScroll = new JPanel(new BorderLayout());
+        panelConScroll.add(scrollPane, BorderLayout.CENTER);
+        return panelConScroll;
+    }
+
+    // Método para crear el panel de amigos
+    private JPanel createAmigosPanel() {
+        JPanel amigosPanel = new JPanel();
+        amigosPanel.setLayout(new BorderLayout());
+        JLabel label = new JLabel("Lista de Amigos", JLabel.CENTER);
+        amigosPanel.add(label, BorderLayout.CENTER);
+        // Aquí puedes agregar más componentes a tu panel de amigos
+        return amigosPanel;
+    }
 
 
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new GUIChat();
-            }
-        });
+        SwingUtilities.invokeLater(() -> new GUIChat());
     }
 }
